e10eca956559bfb1b6091a972c3867fc1be68bbb
rarents1975 <ralf.rentschler@gmx.net>
2017-12-23 14:35:29 +0100

    Update startanzeigen.sh
    Kleine Aufräumarbeiten vorgenommen:
    - Ausgabetexte verbessert
    - Pfadnamen korrigiert

--- startanzeigen.sh
+++ startanzeigen.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
-# Das Programm startet eine slideshow. Dabei werden die auf einem Stick befindlichen jpgs und pdfs verwendet
-#
-#1. Prüfen, wieviele Sticks gelsteckt sind. Wenn <> 1 Stick, wird das vorhandene
-# Verzeichnis Ansicht dargestellt
-unoconv --listener &   # office im Hintergrund starten
+# Das Programm startet eine Slideshow. Dabei werden die auf einem Stick befindlichen jpgs, pdfs und mp4s auf die 
+# Speicherkarte des Raspberry Pi kopiert. Die pdfs werden zuvor in jpgs konvertiert.
+
+# Schritt 1: Pruefen wie viele USB Sticks angeschlossen sind. Wenn <> 1 Stick, wird die Slideshow 
+# mit vorhandenen jpgs und mp4s gestartet. Diese muessen sich in den Verzeichnisses ./Anzeigen und ./Anzeigenmp4 befinden
+
+# Pfad festlegen, in welchem alle Scripte liegen
+pfad="/home/pi/rkups/"
+
+clear
+unoconv --listener &   # Office wird im Hintergrund fuer die Konvertierung gestartet
 echo "---------------------------------------------------------------------------------------------"
 echo "Hintergrund-Programm fuer die Konvertierung von Office- und PDF-Dokumenten wurde gestartet "
 echo "---------------------------------------------------------------------------------------------"
-anzs=$(bash /home/pi/rkups/anzahlsticks.sh )
+anzs=$($pfad"anzahlsticks.sh" )
 echo "Anzahl USB-Sticks:" $anzs
 if [ $anzs != 1 ]
   then
     echo "Keiner oder mehr als ein USB-Stick identifiziert. -> Anzeige wird gestartet"
-     sudo bash /home/pi/rkups/darstellen.sh endlos
+     sudo bash $pfad"darstellen.sh" endlos
     exit 0
 fi
 #2. Wenn ein Stick im Raspberry steckt, dann die Konvertierungen starten
 echo "---------------------------------------------------------------------------------------------"
 echo -e "Es wurde(n)" $anzs "USB-Stick(s) identifiziert -> Bilder und Filme werden neu erzeugt."
-rm -r /home/pi/rkups/Anzeigen/*  # Verzeichnis Anzeigen loeschen
-rm -r /home/pi/rkups/Anzeigenmp4/*  # Verzeichnis Anzeigenmp4 loeschen
-rm -r /home/pi/rkups/Anzeigenurl/*  # Verzeichnis Anzeigenurl loeschen 
+rm -r $pfad"Anzeigen/"*  # Verzeichnis ./Anzeigen loeschen
+rm -r $pfad"Anzeigenmp4/"*  # Verzeichnis ./Anzeigenmp4 loeschen
+rm -r $pfad"Anzeigenurl/"*  # Verzeichnis ./Anzeigenurl loeschen 
 
 # Der komplette Stick wird auf SD kopiert (vorher löschen)
-rm -r /home/pi/rkups/Stick/*  # Verzeichnis Anzeigen loeschen
-cp /media/usb0/* /home/pi/rkups/Stick
-files="/home/pi/rkups/Stick/*"
+rm -r $pfad"Stick/"*  # Verzeichnis ./Anzeigen loeschen
+cp /media/usb0/* $pfad"Stick"
+files=$pfad"Stick/"*
 echo "---------------------------------------------------------------------------------------------"
 echo -e "Die folgenden Dateien befinden sich auf dem USB-Stick: \n", $files # das sind die files auf dem Stick
-cd /home/pi/rkups/Anzeigen
-rename 's/\s/_/g' $files  #alle leerzeichen auf dem Stick werden durch _ ersetzt
+cd $pfad"Anzeigen"
+rename 's/\s/_/g' $files  #alle Leerzeichen der Dateinamen auf auf dem Stick werden durch _ ersetzt
 # Nun werden die Office Files in pdf gewandelt
 echo "---------------------------------------------------------------------------------------------"
 echo -e "Die Leerzeichen in den folgenden Dateinamen wurden entfernt und durch _ ersetzt. \n ", $files # das sind files mit " " -> "_" ersetzt
@@ -46,7 +52,7 @@ do
   fi
 done
 echo "---------------------------------------------------------------------------------------------"
-files="/home/pi/rkups/Stick/*"
+files=$pfad"Stick/*"
 for filename in $files  #jpgs nach Ordner "Anzeigen" kopieren
 do
     typ=${filename:(-4)}  # die letzten 4 Zeichen des filenamens
@@ -54,38 +60,38 @@ do
     if [ $typ == ".pdf" ]
 # Behandlung der pdfs
      then #pdfs werden nach ppm gewandelt
-       cp $filename /home/pi/rkups/Anzeigen    #pdf File wird in Anzeigen gespeichert
+       cp $filename $pfad"Anzeigen"    #pdf File wird in Anzeigen gespeichert
        pdfdatei=${filename##*/}           #Dateiname des pdfs wird ermittelt
-       echo "Zu konvertierende PDF-Datei(en): " $pdfdatei
+       echo "Folgende PDF-Datei wird konvertiert: " $pdfdatei
        pdfdatk=${pdfdatei:0:-4}           # und es werden die zeichen ".pdf" hinten abgeschnitten
        #echo "pdfdatk= " $pdfdatk
        pdftoppm $filename $pdfdatk         # damit wird seitenweise nach ppm gewandelt, der Name bleibt aber gleich
-       rm /home/pi/rkups/Anzeigen/$pdfdatei  #pdfdatei wird wieder gelöscht
+       rm $pfad"Anzeigen/"$pdfdatei  #pdfdatei wird wieder gelöscht
     fi
 echo "---------------------------------------------------------------------------------------------"
 # Behandlung der jpgs und jpegs
     if [ $typ == ".jpg" ] || [ $typ == "jpeg" ]
       then
-        echo -e "Bilder werden auf den Raspberry kopiert: \n " $filename
-        cp $filename /home/pi/rkups/Anzeigen
+        echo -e "JPG-Bild wird auf den Raspberry kopiert: " $filename
+        cp $filename $pfad"Anzeigen"
       fi
 echo "---------------------------------------------------------------------------------------------"
 # Behandlung der url.txt
      if [ ${filename:(-7)}  == "url.txt" ]
      then
-      echo -e  "url.txt mit dem Link auf die Web-Site wir auf den Raspberry kopiert: \n " $filename
-      cp $filename /home/pi/rkups/Anzeigenurl
+      echo -e  "url.txt mit dem Link auf die anzuzeigenden Web-Sites wird auf den Raspberry kopiert: " $filename
+      cp $filename $pfad"Anzeigenurl"
      fi
 echo "---------------------------------------------------------------------------------------------"
 # Behandlung der mp4
      if [ $typ == ".mp4" ] 
      then
-      echo -e "Filme (mp4) werden auf den Raspberry kopiert: \n " $filename
-      cp $filename /home/pi/rkups/Anzeigenmp4
+      echo -e "MP-Movie wird auf den Raspberry kopiert: " $filename
+      cp $filename $pfad"Anzeigenmp4"
 echo "---------------------------------------------------------------------------------------------"
      fi
 done
 
 # nun wird angezeigt
-sudo bash /home/pi/rkups/darstellen.sh endlos
+sudo bash $pfad"darstellen.sh" endlos
 exit 0
